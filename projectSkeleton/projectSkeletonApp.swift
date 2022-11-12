d9eaafc55d8f050cbe5fab10db2303db6ccd955f
Matthew Fitzgerald <79553911+mnfitz-ASU@users.noreply.github.com>
2022-11-12 15:00:10 -0700

    Initial checkin for CSE 335 mobilesat final project (#1)
    Co-authored-by: Matthew Fitzgerald <mn-fitz@hotmail.com>

---
+++ projectSkeleton/projectSkeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectSkeletonApp.swift
+//  projectSkeleton
+//
+//  Created by Matthew Fitzgerald on 10/23/22.
+//
+
+import SwiftUI
+
+@main
+struct projectSkeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
