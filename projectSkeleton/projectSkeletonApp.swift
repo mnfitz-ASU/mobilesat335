0b0d4ee7e0e89f231ed0bc70c4aafecbd371fd84
Matthew Fitzgerald <79553911+mnfitz-ASU@users.noreply.github.com>
2022-11-23 20:14:03 -0700

    Mnfitz/addlist (#5)
    * Implemented coredata into application
    
    * Added multiple satellite adding from list view
    
    Co-authored-by: Matthew Fitzgerald <mn-fitz@hotmail.com>

--- projectSkeleton/projectSkeletonApp.swift
+++ projectSkeleton/projectSkeletonApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct projectSkeletonApp: App {
+    //Core data
+    @StateObject var persistentData = PersistentData()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, persistentData.container.viewContext)
         }
     }
 }
